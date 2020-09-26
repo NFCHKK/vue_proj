8a1784679c7acde9b44c3110f0ebcc743c33f238
NFCHKK <langzihankang@hotmail.com>
2020-09-26 20:55:06 +0800

    Xiao stg (#1)
    * add right popup
    
    * gitignore dist
    
    * add new drawer
    
    * fix drawer error
    
    Co-authored-by: xiaolei <1287148386@qq.com>

--- pages/entrance_manage.vue
+++ pages/entrance_manage.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        <p>welcome entrance</p>
+    <div class="content">
+        <div class="page">
+            <div>
+                <p>welcome entrance</p>
+            </div>
+            <div>
+                <button @click="drawer = !drawer">点我弹框</button>
+            </div>
+        </div>
+        <transition name="fade">
+            <div class="right" v-show="drawer">
+                <div class="drawer-entrance">
+                    <div class="title">入口名称:</div>
+                    <input v-model="dyeing.entrance" placeholder />
+                </div>
+                <div class="drawer-entrance">
+                    <div class="title">应用:</div>
+                    <input v-model="dyeing.app" placeholder />
+                </div>
+                <div class="drawer-entrance">
+                    <div class="title">服务名称:</div>
+                    <input v-model="dyeing.server" placeholder />
+                </div>
+            </div>
+        </transition>
     </div>
 
 </template>
@@ -13,5 +36,60 @@
         components: {
            //  MainApp
         },
+        data: function () {
+            return {
+                dyeing: {
+                    entrance: "",
+                    app: "",
+                    server: "",
+                },
+                drawer: false,
+            }
+        }
     }
 </script>
+<style>
+.content {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    overflow-x: hidden;
+}
+.content .page {
+
+}
+.drawer-entrance {
+    display: flex;
+    margin-bottom: 10px;
+}
+
+.drawer-entrance .title {
+    width: 80px;
+    text-align: right;
+    margin-right: 10px;
+}
+
+.right {
+    padding-top: 40px;
+    padding-right: 40px;
+    border: 1px solid #000;
+    float: right;
+    width: 25%;
+    height: 600px;
+    opacity: 1;
+    position: fixed;
+    right:0;
+    background-color: white;
+}
+.fade-enter-active {
+    transition: all 1s;
+}
+
+.fade-leave-active {
+    transition: all 0s;
+}
+
+.fade-enter, .fade-leve-to {
+    right: -25%;
+}
+</style>
